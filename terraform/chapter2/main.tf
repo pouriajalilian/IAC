c3ae30cbce433225a72bab8dd295bfee5b422048
Your Name <you@example.com>
2025-03-21 11:14:46 +0000

    simple EC2

--- terraform/chapter2/main.tf
+++ terraform/chapter2/main.tf
@@ -1,7 +1,27 @@
-provider "aws" {
-region = "us-east-2"
+
+terraform {
+  required_providers {
+    aws = {
+source  = "hashicorp/aws"
+      version = "5.81.0"   
 }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+
+
+
 resource "aws_instance" "example" {
-ami = "ami-0fb653ca2d3203ac1"
+ami = "ami-0c02fb55956c7d316"
 instance_type = "t2.micro"
+
+tags = {
+Name = "terraform-example"
+}
 }
